cd1ce382abdb9cac71f6848b6a4411c1e57a641e
romainlk <romain.laiking@gmail.com>
2015-03-13 01:24:10 +0000

    Fix gulp task

--- tasks/register/compileAssets.js
+++ tasks/register/compileAssets.js
@@ -2,13 +2,13 @@ module.exports = function (gulp, plugins) {
   gulp.task('compileAssets', function(cb) {
     plugins.sequence(
       'clean:dev',
+      'bower:install',
+      'bower:copy',
       'jst:dev',
       'less:dev',
       'copy:dev',
       'coffee:dev',
       'html2js:dev',
-      'bower:install',
-      'bower:copy',
       cb
     );
   });
